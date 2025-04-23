2664f5acb41e9d5d13167c0c3e3625c9f83e5c0f
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-04-23 04:23:48 -0500

    Actualización al dto Profesor.

--- src/application/dto/profesor/create-profesor.dto.ts
+++ src/application/dto/profesor/create-profesor.dto.ts
@@ -5,12 +5,12 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class createProfesorDto{
     @IsString()
     @IsNotEmpty()
-    @Transform(({ value }) => value.trim())
+    @Transform(({ value }) => (value as string).trim().toLowerCase())
     nombre: string;
 
     @IsString()
     @IsNotEmpty()
-    @Transform(({ value }) => value.trim())
+    @Transform(({ value }) => (value as string).trim().toLowerCase())
     apellido: string;
 
     @IsString()
