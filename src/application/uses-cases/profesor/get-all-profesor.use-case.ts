f3f3d0dd99a815639051dd438aa525ac63b8b643
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-05-15 20:55:11 -0500

    feat(profesor): actualizar caso de uso getAll para usar datos del servicio.

--- src/application/uses-cases/profesor/get-all-profesor.use-case.ts
+++ src/application/uses-cases/profesor/get-all-profesor.use-case.ts
@@ -1,10 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ProfesorService } from '../../../core/services/profesor/profesor.service';
-import { EventEmitter2 } from "@nestjs/event-emitter";
-import { createProfesorDto } from "src/application/dto/profesor";
 import { Result } from "src/shared/domain/result/result";
 import { Profesor } from "src/core/entities/profesor/profesor.entity";
-import { profesorEvent } from "src/domain/events/profesor/profesor-creado.event";
 
 @Injectable()
 export class GetAllProfesorUseCase{
