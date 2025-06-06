22ab7d1987defc954fbb4a30c9720b253e2df725
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-06-05 21:08:22 -0500

    feat(profesor): ajuste de las validaciones

--- src/application/dto/profesor/create-profesor.dto.ts
+++ src/application/dto/profesor/create-profesor.dto.ts
@@ -9,11 +9,14 @@ export class createProfesorDto{
         example: "Juan",
         description: "Nombre del profesor"
     })
-    @MinLength(1, {message: 'El nombre debe tener mas de 1 caracter.'})
+    @MinLength(1, {message: 'El nombre debe tener mas de 1 carácter.'})
     @MaxLength(50, {message: 'El nombre debe tener menos de 50 caracteres.'})
     @IsString({message: 'El nombre debe ser un dato de tipo String.'})
-    @IsNotEmpty({message: 'El nombre no debe estar vacio.'})
-    @Transform(({ value }) => (value as string).trim().toLowerCase().replaceAll(' ',''))
+    @IsNotEmpty({message: 'El nombre no debe estar vacío.'})
+    @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$/, {
+    message: 'El nombre solo puede contener letras, números y espacios',
+    })
+    @Transform(({ value }) => value.trim().replace(/\s+/g, ' '))
     nombre: string;
 
     @ApiProperty({
@@ -23,30 +26,39 @@ export class createProfesorDto{
     @MinLength(8, {message: 'El dni debe tener mas de 7 caracteres.'})
     @MaxLength(20, {message: 'El dni debe tener menos de 14 caracteres.'})
     @IsNumberString({},{message: 'El dni debe ser un dato de tipo Number(Integer).'})
-    @IsNotEmpty({message: 'El dni no debe estar vacio.'})
-    @Transform(({ value }) => (value as string).trim().toLowerCase().replaceAll(' ',''))
+    @IsNotEmpty({message: 'El dni no debe estar vacío.'})
+    @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$/, {
+    message: 'El dni solo puede contener letras, números y espacios',
+    })
+    @Transform(({ value }) => value.trim().replace(/\s+/g, ' '))
     dni: string;
 
     @ApiProperty({ 
-        example: "Perez",
+        example: "Pérez",
         description: "Apellido paterno del profesor"
     })
-    @MinLength(1, {message: 'El apellido paterno debe tener mas de 1 caracter.'})
+    @MinLength(1, {message: 'El apellido paterno debe tener mas de 1 carácter.'})
     @MaxLength(50, {message: 'El apellido paterno debe tener menos de 50 caracteres.'})
     @IsString({message: 'El apellido paterno debe ser un dato de tipo String.'})
-    @IsNotEmpty({message: 'El apellido paterno no debe estar vacio.'})
-    @Transform(({ value }) => (value as string).trim().toLowerCase().replaceAll(' ',''))
+    @IsNotEmpty({message: 'El apellido paterno no debe estar vacío.'})
+    @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$/, {
+    message: 'El apellido paterno solo puede contener letras, números y espacios',
+    })
+    @Transform(({ value }) => value.trim().replace(/\s+/g, ' '))
     apellido_paterno: string;
 
     @ApiProperty({
         example: "Martinez",
         description: "Apellido materno del profesor"
     })
-    @MinLength(1, {message: 'El apellido materno debe tener mas de 1 caracter.'})
+    @MinLength(1, {message: 'El apellido materno debe tener mas de 1 carácter.'})
     @MaxLength(50, {message: 'El apellido materno debe tener menos de 50 caracteres.'})
     @IsString({message: 'El apellido materno debe ser un dato de tipo String.'})
-    @IsNotEmpty({message: 'El apellido materno no debe estar vacio.'})
-    @Transform(({ value }) => (value as string).trim().toLowerCase().replaceAll(' ',''))
+    @IsNotEmpty({message: 'El apellido materno no debe estar vacío.'})
+    @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$/, {
+    message: 'El apellido materno solo puede contener letras, números y espacios',
+    })
+    @Transform(({ value }) => value.trim().replace(/\s+/g, ' '))
     apellido_materno: string;
 
     @ApiProperty({
@@ -61,10 +73,10 @@ export class createProfesorDto{
         example: "juan@correo.com",
         description: "Email del profesor"
     })
-    @MinLength(1, {message: 'El email debe tener mas de 1 caracter.'})
+    @MinLength(1, {message: 'El email debe tener mas de 1 carácter.'})
     @MaxLength(100, {message: 'El email debe tener menos de 100 caracteres.'})
-    @IsEmail({},{message: 'Debe ingresar un correo electronico valido.'})
-    @IsNotEmpty({message: 'El email no debe estar vacio.'})
+    @IsEmail({},{message: 'Debe ingresar un correo electronico válido.'})
+    @IsNotEmpty({message: 'El email no debe estar vacío.'})
     @Transform(({ value }) => (value as string).trim().toLowerCase().replaceAll(' ',''))
     email:string;
 
