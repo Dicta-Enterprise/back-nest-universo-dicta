cbf18a217112a641d45d6ce211f7ece872ada67f
Sebastian Quevedo <squevlav@gmail.com>
2026-01-29 12:22:43 -0500

    git push origin-u feature/24/mejora-del-sidebar

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { GalaxiasModule } from './infraestructure/http/galaxia/galaxias.module';
 import { PlanetasModule } from './infraestructure/http/planeta/planetas.module';
 import { CategoriaModule } from './infraestructure/http/categoria/categorias.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { MenuModule } from './infraestructure/http/menu/menu.module';
 import { MenuSidebarModule } from './infraestructure/menu-sidebar/menuSidebar.module';
 import { AzureModule } from './infraestructure/http/azure/azure.module';
 import { ProfesorModule } from './infraestructure/http/profesor/profesor.module';
@@ -16,7 +15,6 @@ import { ProfesorModule } from './infraestructure/http/profesor/profesor.module'
   imports: [
     EventEmitterModule.forRoot(),
     CursosModule,
-    MenuModule,
     PlanetasModule,
     GalaxiasModule,
     CategoriaModule,
