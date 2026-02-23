52d34b0cb3e6c290872f4508287382e7587c72d4
AlesisxHz-afk <M3Alesisx@gmail.com>
2026-02-22 19:19:29 -0500

    Refactor: se quito el unique a slug y a titulo y se arreglo el metodo get

--- src/core/repositories/landing-page/landing-page.repository.ts
+++ src/core/repositories/landing-page/landing-page.repository.ts
@@ -7,9 +7,9 @@ import { LandingPage } from 'src/core/entities/landing-page/landing-page.entity'
 export interface LandingPageRepository {
   findById(id: string): Promise<LandingPage | null>;
   findByTitulo(titulo: string): Promise<LandingPage | null>;
-  findAllActive(): Promise<LandingPage[]>;
+  findAll(): Promise<LandingPage[]>;
   save(landingPage: LandingPage): Promise<LandingPage>;
   update(id: string, landingPage: Partial<LandingPage>): Promise<LandingPage>;
   delete(id: string, estado: boolean): Promise<LandingPage>;
+  findBySlug(slug: string): Promise<LandingPage | null>;
 }
-  
